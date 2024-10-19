112a1fe4198d8726b735945167e989dbf16af8b0
Huanzhong Chen <57387699+0XMY0@users.noreply.github.com>
2024-10-19 23:41:06 +0800

    .
    generate more candidate trajectory for y motion

--- legged_gym/tests/lipm_cyclic_test.py
+++ legged_gym/tests/lipm_cyclic_test.py
@@ -1,95 +1,185 @@
 import numpy as np
 from math import exp
+import torch
+import matplotlib.pyplot as plt
+from scipy.special import erf
+from scipy.optimize import curve_fit
 
 class LIPM:
-    def __init__(self, height, T, disy):
+    def __init__(self, height, freq, disy, phasediff, swingphasespan):
         self.height = height
-        self.disy = 0.10
+        self.disy = disy
         self.g = 9.81
         self.w0 = (self.g/self.height)**0.5
-        self.T = T
-        self.dsupphase = 0.111111
-        self.qy0 = self._getqy0_dsup()
+        self.freq = freq # for a whole cycle containing two steps
+        self.phasediff = phasediff # phase difference between two feet
+        self.swingphasespan = swingphasespan # phase span for swing phase
+        self.nseg = 200
 
-    def _getbound(self):
-        bounds = [0, 0]
-        Kp = 10.0
-        t = 0
-        step = 0
-        sup = 'R'
-        y = [-0.025, 0.0] # pos vel
-        y[1] = (-self.qy0 - y[0]) * self.w0
-        ys = []
-        for i in range(0, np.floor(6 * self.T / 0.004).astype(int)):
-            t += 0.004
-            if t > self.T:
-                sup = 'R' if sup == 'L' else 'L'
-                t -= self.T
-                step += 1
-            qy0 = self.qy0 if sup == 'L' else -self.qy0
-            anky = self.disy if sup == 'L' else -self.disy
-            qyd = (qy0 - anky) * np.exp(self.w0 * t) + anky
-            qynow = y[0] + y[1] / self.w0
-            if t < (1.0 - self.dsupphase) * self.T:
-                zmpy = Kp * (qynow - qyd) + anky
-            else:
-                zmpy = Kp * (qynow - qyd) - anky
-            ddy = self.g / self.height * (y[0] - zmpy)
-            y[0] += y[1] * 0.004 + 0.5 * ddy * 0.004**2
-            y[1] += ddy * 0.004
-            if(step >= 4 and step <= 5):
-                bounds[0] = min(bounds[0], y[1])
-                bounds[1] = max(bounds[1], y[1])
-            ys.append([y[0], y[1], ddy, zmpy])
-
-        with open('lipm_cyclic_test.csv', 'w') as f:
-            f.write('t,y,dy,ddy,zmpy\n')
-            for y in ys:
-                f.write(','.join(map(str, y)) + '\n')
-        return (bounds[1] - bounds[0]) / 2.0
+    def _getswingsignal(self, phase):
+        def _getphase(x, mu_A, mu_B):
+            def normal_cdf(x, mu, sigma):
+                return 0.5 * (1 + erf((x - mu) / (sigma * 2.0)))
+            P1 = normal_cdf(x, mu_A, 0.01) * (1 - normal_cdf(x, mu_B, 0.01))
+            if(mu_A > 0.2 and mu_B < 0.8):
+                return P1
+            if(mu_A <= 0.2):
+                P2 = normal_cdf(x - 1, mu_A, 0.01) * (1 - normal_cdf(x - 1, mu_B, 0.01))
+                return P1 + P2
+            if(mu_B >= 0.8):
+                P3 = normal_cdf(x + 1, mu_A, 0.01) * (1 - normal_cdf(x + 1, mu_B, 0.01))
+                return P1 + P3
+            return P1
+        return _getphase(phase, 0, self.swingphasespan), _getphase(phase, self.phasediff, self.phasediff + self.swingphasespan)
     
-    def _getqy0_dsup(self) -> float:
-        def _getqend(q0):
-            q1 = (q0 - self.disy) * exp(self.w0 * self.T * (1 - self.dsupphase)) + self.disy
-            q2 = (q1 - -self.disy) * exp(self.w0 * self.T * self.dsupphase) + -self.disy
-            return q2
-        q0 = 0.05
+    def _getzmp(self, phase):
+        swingl, swingr = self._getswingsignal(phase)
+        stancel = 1 - swingl
+        stancer = 1 - swingr
+        return self.disy * stancel + -self.disy * stancer
+    
+    def _getqend(self, q0):
+        dt = 1.0 / self.freq / self.nseg
+        q = q0
+        qslog = []
+        for i in range(self.nseg):
+            zmp = self._getzmp(i / self.nseg)
+            qslog.append([q, zmp])
+            q = (q - zmp) * exp(self.w0 * dt) + zmp
+        return q, qslog
+    
+    def _getq0y(self):
+        q0 = 0.0
+        f_grad_last = 10
+        step = 1e-3
         while(1):
-            qnow = q0
-            qend = _getqend(q0)
-            # min f = (qend + q0)^2
-            qend_add = _getqend(q0 + 1e-5)
-            qend_sub = _getqend(q0 - 1e-5)
+            qend, _ = self._getqend(q0)
+            qend_add, _ = self._getqend(q0 + 1e-5)
+            qend_sub, _ = self._getqend(q0 - 1e-5)
             qend_grad = (qend_add - qend_sub) / 2e-5
-            f_grad = 2 * (qend + q0) * (qend_grad + 1)
-            q0 -= f_grad * 1e-3
+            f_grad = 2 * (qend - q0) * (qend_grad - 1)
+            if(abs(f_grad) > abs(f_grad_last) or (f_grad * f_grad_last < 0)):
+                step /= 2
+            q0 -= np.clip(f_grad * step, -0.01, 0.01)
+            f_grad_last = f_grad
+            # print(q0, f_grad)
             if(abs(f_grad) < 1e-5):
                 break
         return q0
+    
+    def _gettra(self):
+        q0 = self._getq0y()
+        qend, qslog = self._getqend(q0)
+        Kp = 10.0
+        y = [0, 0]
+        y[1] = (q0 - y[0]) * self.w0
+        ys = []
+        for i in range(0, 3 * self.nseg):
+            step, ii = divmod(i, self.nseg)
+            zmpd = qslog[ii][1]
+            qnow = y[0] + y[1] / self.w0
+            qd = qslog[ii][0]
+            zmp = -Kp * (qnow - qd) + zmpd
+            dt = 1.0 / self.freq / self.nseg
+            ddy = self.g / self.height * (zmp - y[0])
+            y[0] += y[1] * dt + 0.5 * ddy * dt * dt
+            y[1] += ddy * dt
+            if step == 2:
+                ys.append([i * dt, y[0].copy(), y[1].copy(), ddy, zmp, zmpd])
+        return ys
 
-# data = []
-# for i in range(0, 10000):
-#     height_rand_bounds = [0.4, 0.8]
-#     T_rand_bounds = [0.2, 1.0]
-#     lipm = LIPM(np.random.uniform(height_rand_bounds[0], height_rand_bounds[1]), np.random.uniform(T_rand_bounds[0], T_rand_bounds[1]), 0.10)
-#     data.append([lipm.height, 1.0 / (2 * lipm.T), lipm._getbound()])
-# with open('lipm_cyclic_test.csv', 'w') as f:
-#     f.write('height,T,disy\n')
-#     for d in data:
-#         f.write(','.join(map(str, d)) + '\n')
+def generatedata():
+    freq_bounds = [0.5, 2.5]
+    phasediff_range = [0.0, 0.5]
+    swingphasespan_range = [0.3, 0.8]
+    len = 1000
+    data = []
+    for i in range(len):
+        lipm = LIPM(
+            height=0.55,
+            freq=np.random.uniform(freq_bounds[0], freq_bounds[1]),
+            disy=0.1,
+            phasediff=np.random.uniform(phasediff_range[0], phasediff_range[1]),
+            swingphasespan=np.random.uniform(swingphasespan_range[0], swingphasespan_range[1]))
+        print(i, 'freq = ', lipm.freq, 'phasediff = ', lipm.phasediff, 'swingphasespan = ', lipm.swingphasespan)
+        vel = np.array(lipm._gettra())[:, 2]
+        datanow = [vel[0], vel[int(lipm.nseg / 6)], vel[int(lipm.nseg / 3)], vel[int(lipm.nseg / 2)], vel[int(lipm.nseg / 3 * 2)], vel[int(lipm.nseg / 6 * 5)]]
+        data.append([lipm.freq, lipm.phasediff, lipm.swingphasespan] + datanow)
+        with open('data.csv', 'a') as f:
+            f.write(','.join(map(str, data[-1])) + '\n')
+    return data
 
-# fitted bound for vy from MATLAB: f(x, y) = 0.9761 - 1.106 * x - 0.2678 * y + 0.531 * x^2 + 0.06972 * x * y + 0.03481 * y^2, where x = height, y = frequency, f = vy bound
+def _fit_function2(x, a0, a1, a2, a3):
+    return a0 + a1 * x[:, 0] + a2 * x[:, 1] + a3 * x[:, 2]
 
-# test
-# height_rand_bounds = [0.4, 0.8]
-# T_rand_bounds = [0.2, 1.0]
-# lipm1 = LIPM(np.random.uniform(height_rand_bounds[0], height_rand_bounds[1]), np.random.uniform(T_rand_bounds[0], T_rand_bounds[1]), 0.10)
-# lipm2 = LIPM(0.55, 0.33, 0.10)
+def fitdata2(x, y):
+    param = curve_fit(_fit_function2, x, y)
+    return param
 
-# x = lipm1.height
-# y = 1.0 / (2 * lipm1.T)
-# print('original bound:', lipm1._getbound())
-# print('fitted bound:', 0.9761 - 1.106 * x - 0.2678 * y + 0.531 * x**2 + 0.06972 * x * y + 0.03481 * y**2)
+# # test _getswingsignal and _getzmp
+# lipm = LIPM(height=0.55, freq=1.5, disy=0.1, phasediff=0.5, swingphasespan=0.45)
+# i = torch.linspace(0, 1, 1000)
+# swingl, swingr = lipm._getswingsignal(i)
+# # plot swingl, swingr in subplot 1
+# fig, axs = plt.subplots(2)
+# axs[0].plot(i, swingl, label='swingl')
+# axs[0].plot(i, swingr, label='swingr')
+# axs[0].legend()
+# # plot zmp in subplot 2
+# zmp = lipm._getzmp(i)
+# axs[1].plot(i, zmp, label='zmp')
+# axs[1].legend()
+# plt.show()
 
-lipm2 = LIPM(0.55, 0.3333333, 0.10)
-print(lipm2._getbound())
+# test _getqend and _getq0y
+# lipm = LIPM(height=0.55, freq=1.5, disy=0.1, phasediff=0.5, swingphasespan=0.45)
+# q0 = lipm._getq0y()
+# qend, qslog = lipm._getqend(q0)
+# # plot qslog in single plot
+# fig, axs = plt.subplots(1)
+# axs.plot(qslog, label='q')
+# axs.legend()
+# plt.show()
+
+# test _gettra
+# lipm = LIPM(height=0.55, freq=1.5, disy=0.1, phasediff=0.1, swingphasespan=0.45)
+# ys = lipm._gettra()
+# # plot ys in 2*2 subplots
+# fig, axs = plt.subplots(2, 3)
+# ys = np.array(ys)
+# t = ys[:, 0] - ys[0, 0]
+# axs[0, 0].plot(t, ys[:, 1], label='y')
+# axs[0, 0].legend()
+# axs[0, 1].plot(t, ys[:, 2], label='dy')
+# axs[0, 1].legend()
+# axs[0, 2].plot(t, ys[:, 3], label='ddy')
+# axs[0, 2].legend()
+# axs[1, 0].plot(t, ys[:, 4], label='zmp')
+# axs[1, 0].legend()
+# axs[1, 1].plot(t, ys[:, 5], label='zmpd')
+# axs[1, 1].legend()
+# plt.show()
+
+# test generatedata
+data = generatedata()
+data = np.array(data)
+
+# test _read_and_fit_data
+# data = np.loadtxt('data.csv', delimiter=',')
+# x = data[:, :3]
+# y1 = data[:, 3]
+# idx = np.argsort(x[:, 0])
+# x = x[idx]
+# y1 = y1[idx]
+# param = fitdata2(x, y1)
+# # plot
+# y1_fit = _fit_function2(x, *param[0])
+# plt.figure(figsize=(10, 6))
+# plt.plot(y1, label='Raw Data', color='blue')
+# plt.plot(y1_fit, label='Fitted Curve', color='red', linestyle='--')
+# plt.xlabel('Index')
+# plt.ylabel('Data')
+# plt.title('Comparison of Raw Data and Fitted Curve')
+# plt.legend()
+# plt.grid()
+# plt.show()
